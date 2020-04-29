3f1b1b1fb6b666d87cfc95e8b80bf65d4f2796ba
Denny Britz <dennybritz@gmail.com>
2020-04-29 12:53:13 +0200

    Add index to join table

--- migrations/2020-04-29-094327_create_citations/down.sql
+++ migrations/2020-04-29-094327_create_citations/down.sql
@@ -1,1 +1,3 @@
 DROP TABLE citations;
+DROP INDEX citations_from_paper_idx;
+DROP INDEX citations_to_paper_idx;
--- migrations/2020-04-29-094327_create_citations/up.sql
+++ migrations/2020-04-29-094327_create_citations/up.sql
@@ -3,3 +3,6 @@ CREATE TABLE citations (
   to_paper VARCHAR NOT NULL,
   PRIMARY KEY(from_paper, to_paper)
 );
+
+CREATE INDEX citations_from_paper_idx ON citations (from_paper);
+CREATE INDEX citations_to_paper_idx ON citations (to_paper);
