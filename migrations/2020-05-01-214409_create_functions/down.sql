414c5a7c19d56825ae71a16052a83b6b62fd7945
Denny Britz <dennybritz@gmail.com>
2020-05-27 20:12:52 +0200

    Add cited_by query functionality

--- migrations/2020-05-01-214409_create_functions/down.sql
+++ migrations/2020-05-01-214409_create_functions/down.sql
@@ -1,3 +1,3 @@
 DROP FUNCTION cites;
 DROP FUNCTION num_citations;
-
+DROP FUNCTION cited_by;
--- migrations/2020-05-01-214409_create_functions/up.sql
+++ migrations/2020-05-01-214409_create_functions/up.sql
@@ -7,6 +7,14 @@ RETURNS SETOF papers AS $$
   LIMIT limit_
 $$ LANGUAGE SQL STABLE;
 
+CREATE FUNCTION cited_by(paper_row papers, limit_ integer)
+RETURNS SETOF papers AS $$
+  SELECT p2.* FROM papers p1
+  JOIN papers p2 ON p2.id=ANY(p1.in_citations)
+  WHERE p1.id = paper_row.id AND p2.id != paper_row.id
+  ORDER BY array_length(p2.in_citations, 1) DESC
+  LIMIT limit_
+$$ LANGUAGE SQL STABLE;
 
 CREATE FUNCTION num_citations(paper_row papers)
 RETURNS integer AS $$
